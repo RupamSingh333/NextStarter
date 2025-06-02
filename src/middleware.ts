146f4519f24893fbea05ff851aee4c5a3f9d3d79
Rupam Singh <rupamkumar333@gmail.com>
2025-06-03 00:31:27 +0530

    fied bug

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 // List of public routes that don't require authentication
-const publicRoutes = ['/', '/signin', '/auth/signin', '/auth/signup'];
+// const publicRoutes = ['/', '/signin', '/auth/signin', '/auth/signup'];
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
