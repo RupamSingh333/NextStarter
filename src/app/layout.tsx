b726967326bf8b408db366cbaf7da1700d9a8e8d
Rupam Singh <rupamkumar333@gmail.com>
2025-06-18 02:27:10 +0530

    doen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Toaster } from 'react-hot-toast';
 import { LoadingProvider } from "@/context/LoadingContext";
 import LoadingScreen from "@/components/common/LoadingScreen";
 import TopLoadingBar from "@/components/common/TopLoadingBar";
+import { Suspense } from 'react';
 
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
@@ -27,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
-        <TopLoadingBar />
+        <Suspense fallback={null}>
+          <TopLoadingBar />
+        </Suspense>
         <AuthProvider>
           <ThemeProvider>
             <SidebarProvider>
